d7e2c11546c80cbb12861a407bb24b6fa0e57427
Joud <joud.hasan85@gmail.com>
2023-09-01 17:44:13 +0200

    FIXED

--- index.js
+++ index.js
@@ -22,14 +22,64 @@ let requestTime = (req, res, next) => {
 app.use(myLogger);
 app.use(requestTime);
 
+let movies = [
+  {
+    Title: "The Lives of Others",
+    Description:
+      "In 1984, when an East German Stasi agent is ordered to spy on a writer, the more he discovers about the writer's disloyalty, the more his own faith in the Communist system is shaken.",
+    Genre: {
+      Name: "Drama",
+      Description:
+        "In film and television, drama is a category or genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.",
+    },
+    Director: {
+      Name: "Florian Henckel",
+    },
+    ImageURL: "https://images.app.goo.gl/tukE2gj2t1MPqeL7A",
+    Featured: false,
+  },
+  {
+    Title: "Good Bye, Lenin!",
+    Description:
+      "In 1990, to protect his fragile mother from a fatal shock after a long coma, a young man must keep her from learning that her beloved nation of East Germany as she knew it has disappeared",
+    Genre: {
+      Name: "Drama-Comedy",
+      Description:
+        "Comedy is a genre of dramatic performance having a light or humorous tone that depicts amusing incidents and in which the characters ultimately triumph over adversity.",
+    },
+    Director: {
+      Name: "Wolfgang Becker",
+    },
+    ImageURL: "https://images.app.goo.gl/FQ9XLDjW5DRKriBv8",
+    Featured: false,
+  },
+  // Add other movie objects here...
+  {
+    Title: "The Post",
+    Description:
+      "The Post is a 2017 American semi-fiction historical political thriller film about The Washington Post and the publication of the Pentagon Papers.",
+    Genre: {
+      Name: "Historical drama",
+      Description:
+        "Historical drama (also period drama, period piece or just period) is a dramatic work set in a past time period, usually used in the context of film and television, which presents historical events and characters with varying degrees of fictional elements such as creative dialogue or fictional scenes which aim to compress separate events or illustrate a broader factual narrative.",
+    },
+    Director: {
+      Name: "Steven Spielberg",
+    },
+    ImageURL: "https://images.app.goo.gl/xfzoLKC4hV4Z4nFL9",
+    Featured: false,
+  },
+  // Add other movie objects here...
+];
+
 app.get("/movies", (req, res) => {
- res.status(200).send.json (movies);
+  res.status(200).json(movies);
 });
 
 app.get("/", (req, res) => {
-  // Return a default textual response
   res.send("Welcome to my movie app!");
 });
+
 app.use(express.static("public"));
 
 app.use((err, req, res, next) => {
@@ -38,15 +88,15 @@ app.use((err, req, res, next) => {
 });
 
 // read
-app.get("movie/:title"),(req.res) => {
-  const {title} = req.params;
+app.get("/movie/:title", (req, res) => {
+  const { title } = req.params;
   const movie = movies.find((movie) => movie.title === title);
   if (movie) {
     res.status(200).json(movie);
   } else {
     res.status(400).send("Movie not found");
   }
-};
+});
 //read
 app.get("/movies/gerne/:gerneName", (req, res) => {
   const { gerneName } = req.params;
@@ -60,7 +110,7 @@ app.get("/movies/gerne/:gerneName", (req, res) => {
 //read
 app.get("/movies/director/:directorName", (req, res) => {
   const { directorName } = req.params;
-  const director = movies.find(movies.director.name === directorName).director;
+  const director = movies.find(movies.director.Name === directorName).director;
   if (director) {
     res.status(200).json(director);
   } else {
@@ -86,28 +136,28 @@ let users = [
 //create
 app.post("/users", (req, res) => {
   const newUser = req.body;
-if (newUser.name) {
-  newUser.id = uuid.v4();
-  users.push(newUser);
-  res.status(201).json(newUser);
-} else {
-  res.status(400).send("User must have a name");
-}
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser);
+  } else {
+    res.status(400).send("User must have a name");
+  }
 });
 //update
 app.put("/users/:id", (req, res) => {
   const { id } = req.params;
   const { name } = req.body;
-  let users.find((user) => user.id === id);
+  let user = users.find((user) => user.id === id);
   if (user) {
-    user.name = update user.name;
+    user.name = user.name;
     res.status(200).json(user);
   } else {
     res.status(400).send("User not found");
   }
 });
 //create
-app.post ("/users,:id/:movieTitle"), (req, res) => {
+app.post("/users/:id/:movieTitle", (req, res) => {
   const { id, movieTitle } = req.params;
   let user = users.find((user) => user.id === id);
   if (user) {
@@ -116,28 +166,30 @@ app.post ("/users,:id/:movieTitle"), (req, res) => {
   } else {
     res.status(400).send("User not found");
   }
-};
+});
 //delete
-app.delete ("/users/:id/:movieTitle"), (req, res) => {
+app.delete("/users/:id/:movieTitle", (req, res) => {
   const { id, movieTitle } = req.params;
   let user = users.find((user) => user.id === id);
   if (user) {
-    user.favoriteMovies = user.favoriteMovies.filter(title => title !== movieTitle);
+    user.favoriteMovies = user.favoriteMovies.filter(
+      (title) => title !== movieTitle
+    );
     res.status(200).json(user);
   } else {
     res.status(400).send("User not found");
   }
-};
+});
 //delete
 app.delete("/users/:id", (req, res) => {
   const { id } = req.params;
-  users = users.find ((user) => user.id !== id);
-if (user) {
-  users = users.filter((user) => user.id !== id);
-  res.status(200).send ("User deleted");
-} else {
-  res.status(400).send("User not found");
-}
+  users = users.find((user) => user.id !== id);
+  if (user) {
+    users = users.filter((user) => user.id !== id);
+    res.status(200).send("User deleted");
+  } else {
+    res.status(400).send("User not found");
+  }
 });
 
 const port = 8080; // Choose a port number
